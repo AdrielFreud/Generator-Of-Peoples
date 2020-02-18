a40a0a9cff03ea74a2ef2cdee33a969fe85db7cc
AdrielFreud <gamerbra39@gmail.com>
2020-02-18 12:21:20 -0300

    Add functions for extract data

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
 var tableElement = document.querySelector("table");
-
-<<<<<<< HEAD
 var todos = ["name", "surname", "gender", "region", "age", "phone", "email", "password", "photo", "off"];
-=======
-var todos = ["name", "surname", "gender", "region", "off"];
->>>>>>> 7ead2a97883aa7b8bff518a6942c4701fa604ad5
-
 var minhaPromise = function(){
 	return new Promise(function(resolve, reject) {
 		var xhr = new XMLHttpRequest();
-<<<<<<< HEAD
 		// https://uinames.com/api/?ext&region=Brazil
-		xhr.open('GET', 'https://uinames.com/api/?ext&region=Brazil&amount=250');
-=======
-
-		xhr.open('GET', 'https://uinames.com/api/?amount=250');
->>>>>>> 7ead2a97883aa7b8bff518a6942c4701fa604ad5
+		xhr.open('GET', 'https://uinames.com/api/?ext&region=Brazil&amount=100');
 		xhr.send(null);
 
 		xhr.onreadystatechange = function(){
@@ -33,19 +22,13 @@ var minhaPromise = function(){
 minhaPromise()
 	.then(
 	function(response){
-		//istElement.innerHTML = '';
 		var peoples = response;
-
 		for(p of peoples){
 			var trElement = document.createElement("tr");
 			for(todo of todos){
 				if(p[todo]){
 					var tdElement = document.createElement("td");
 					var textTodo = document.createTextNode(p[todo]);
-<<<<<<< HEAD
-
-=======
->>>>>>> 7ead2a97883aa7b8bff518a6942c4701fa604ad5
 					tdElement.appendChild(textTodo);
 					trElement.appendChild(tdElement);
 					tableElement.appendChild(trElement);	
